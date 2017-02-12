5e174e2db46c473ad436c2ef4b1332f6e2bed8c6
Jacob Alberty <jacob.alberty@gmail.com>
2017-02-11 23:49:22 -0600

    Oops, should have been looking for /var/firebird/system/security3.fdb not ${PREFIX}/security3.fdb

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-if [ ! -f "${PREFIX}/security3.fdb" ]; then
+if [ ! -f "/var/firebird/system/security3.fdb" ]; then
     if [ -z ${ISC_PASSWORD} ]; then
         ISC_PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
     fi
