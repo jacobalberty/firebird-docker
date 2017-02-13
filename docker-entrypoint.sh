539acb51b3f621bba3f12edfb3a9727b32a3b0ba
Jacob Alberty <jacob.alberty@vetmanage.com>
2017-02-13 12:37:51 -0600

    Allow ISC_PASSWORD_FILE variable to override ISC_PASSWORD and set it from a docker secret. While adding that I also set it so it only outputs the password if it is using a random one.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
 set -e
 
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+    local var="$1"
+    local fileVar="${var}_FILE"
+    local def="${2:-}"
+    if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+        echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+        exit 1
+    fi
+    local val="$def"
+    if [ "${!var:-}" ]; then
+        val="${!var}"
+    elif [ "${!fileVar:-}" ]; then
+        val="$(< "${!fileVar}")"
+    fi
+    export "$var"="$val"
+    unset "$fileVar"
+}
+
 if [ ! -f "/var/firebird/system/security3.fdb" ]; then
     cp ${PREFIX}/security3.fdb /var/firebird/system/security3.fdb
+    file_env 'ISC_PASSWORD'
     if [ -z ${ISC_PASSWORD} ]; then
-        ISC_PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+       ISC_PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+       echo "setting 'SYSDBA' password to '${ISC_PASSWORD}'"
     fi
 
     ${PREFIX}/bin/isql -user sysdba employee <<EOL
@@ -30,8 +54,6 @@ ISC_PASSWD=${ISC_PASSWORD}
 #
 
 EOL
-
-echo "set 'SYSDBA' password to '${ISC_PASSWORD}'"
 
 fi
 
